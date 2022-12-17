92e82892e7730d178b32f0921712b55071741754
Lennox Charles <lennyaiko17@gmail.com>
2022-12-17 20:33:16 +0100

    server is up and ready

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ChatApi
+# Chatify
--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+
+const app = express()
+const PORT = 5050
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => res.send("Hello this is Chatify."))
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
